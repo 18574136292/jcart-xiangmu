b950075cdf439b976e18cad0e54e8609a346aabf
LAPTOP-3T45MA9E\Lenovo <18574136292@163.com>
2020-03-24 11:17:21 +0800

    update

--- sourceTest-1705S-store/src/main/java/io/itcast/cfc/MyApp.java
+++ sourceTest-1705S-store/src/main/java/io/itcast/cfc/MyApp.java
@@ -3,9 +3,11 @@ package io.itcast.cfc;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("io.itcast.cfc.dao")
+@EnableScheduling
 public class MyApp {
     public static void main(String[] args) {
         SpringApplication.run(MyApp.class,args);
